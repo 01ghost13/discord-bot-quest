d7228ccd0c6e9a2f6a3a7fa91458b606781268ca
Unknown <orkich@gmail.com>
2019-06-02 23:04:31 +0400

    [add] Handling env vars

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	"prefix": "!",
+	"development":  {
+		"BOT_TOKEN": process.env.BOT_TOKEN
+	},
+	"production": {
+		"BOT_TOKEN": process.env.BOT_TOKEN
+	}
+}
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
+const config = require('./config.js');
+const environment = process.env.NODE_ENV || 'development';
+const environmentConfig = config[environment];
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login(config.token);
+client.login(environmentConfig.BOT_TOKEN);
